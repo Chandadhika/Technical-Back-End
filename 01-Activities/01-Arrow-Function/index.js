8d988cbfc6f0403313dee879115504bfb95d0468
ANarngAung <anarngaung2-17@gmail.com>
2024-10-27 21:22:30 +0630

    added map loop array

--- 01-Activities/01-Arrow-Function/index.js
+++ 01-Activities/01-Arrow-Function/index.js
@@ -10,3 +10,23 @@ var funnyCase = string => {
 
 // Prints `yOu cAn't jUst Do wHaTeVeR YoU WaNt aLl tHe tImE!` //ထွက်လာသည့်ပုံစံများ
 console.log(funnyCase("You can't just do whatever you want all the time!"));// string-အတွက် ထည့်ထားခြင်း
+
+
+// 2. Map lets you loop over an array and modify the elements inside
+var map = (arr, cb) => {
+    var result = [];
+    for (var index = 0; index < arr.length; index++) {
+        var currentElement = arr[index];
+        result.push(cb(currentElement, index));
+    }
+    return result;
+};
+
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+var doubled = map(numbers, element => {
+    return element * 2; // 2-နဲ့ မြှောက်
+});
+
+// Prints `[2, 4, 6, 8, 10, 12, 14, 16, 18, 20]`
+console.log(doubled);
