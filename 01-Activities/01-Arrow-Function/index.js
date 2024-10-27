ab80f62f6c54bdfff9106ed91a8f35b5f7d8174e
ANarngAung <anarngaung2-17@gmail.com>
2024-10-27 07:33:58 +0630

    added string condition

--- 01-Activities/01-Arrow-Function/index.js
+++ 01-Activities/01-Arrow-Function/index.js
@@ -1,1 +1,12 @@
 //1. funnyCase makes each letter in a string the opposite case as the one before
+var funnyCase = string => {
+    var newString = '';
+    for (var i = 0; i < string.length; i++) {
+        if (i % 2 === 0) newString += string[i].toLowerCase(); // စားလို့ ပြတ်တဲ့ဟာကို toLowerCaseပြောင်းပြီး ထည့်ပေးပါ
+        else newString += string[i].toUpperCase(); // စားလို့ မပြတ်တဲ့ဟာကို toUpperCase ပြောင်းပြီး ထည့်ခြင်း
+    }
+    return newString;
+};
+
+// Prints `yOu cAn't jUst Do wHaTeVeR YoU WaNt aLl tHe tImE!` //ထွက်လာသည့်ပုံစံများ
+console.log(funnyCase("You can't just do whatever you want all the time!"));// string-အတွက် ထည့်ထားခြင်း
